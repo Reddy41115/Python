3d01e1d6da4934a3af26c670c6f7c7771189bb6f
M Reddappa Yadav <srym41115@gmail.com>
2025-10-11 23:37:45 +0530

    exercise2.py

---
+++ MultipleInheritance/exercise2.py
@@ -0,0 +1,20 @@
+# Base Class 1
+class __________:
+    def __________(self):
+        print("[Walker] Walking...")
+
+# Base Class 2
+class __________:
+    def __________(self):
+        print("[Runner] Running...")
+
+# Derived Class
+class __________(__________, __________):
+    def __________(self):
+        print("[Athlete] Competing!")
+
+# Object creation and method calls
+athlete = __________()
+athlete.__________()
+athlete.__________()
+athlete.__________() 
