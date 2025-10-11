210b575b4768a6d5f0a4bab4c7dbd1ff5328d08a
M Reddappa Yadav <srym41115@gmail.com>
2025-10-11 23:38:13 +0530

    exercise3.py

---
+++ MultipleInheritance/exercise3.py
@@ -0,0 +1,20 @@
+# Base Class 1
+class __________:
+    def __________(self):
+        print("[Painter] Painting...")
+
+# Base Class 2
+class __________:
+    def __________(self):
+        print("[Singer] Singing...")
+
+# Derived Class
+class __________(__________, __________):
+    def __________(self):
+        print("[Artist] Performing!")
+
+# Object creation and method calls
+artist = __________()
+artist.__________()
+artist.__________()
+artist.__________() 
