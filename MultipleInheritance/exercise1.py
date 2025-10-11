593a7c4d93807c1d2da2303637021ff7e00ef63b
M Reddappa Yadav <srym41115@gmail.com>
2025-10-11 23:37:13 +0530

    exercise1.py

---
+++ MultipleInheritance/exercise1.py
@@ -0,0 +1,29 @@
+# Base Class 1
+class __________:
+    def __init__(self):
+        print("[Flyer] Can fly!")
+    def __________(self):
+        print("[Flyer] Flying...")
+
+# Base Class 2
+class __________:
+    def __init__(self):
+        print("[Swimmer] Can swim!")
+    def __________(self):
+        print("[Swimmer] Swimming...")
+
+# Derived Class
+class __________(__________, __________):
+    def __init__(self):
+        __________.__init__(self)
+        __________.__init__(self)
+        print("[Duck] Initialized!")
+
+    def __________(self):
+        print("[Duck] Quacking!")
+
+# Object creation and method calls
+donald = __________()
+donald.__________()
+donald.__________()
+donald.__________() 
